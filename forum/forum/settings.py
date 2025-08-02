c3e6529b89259ccb3610308e04f90aebc9024812
kuznetsovroman41 <romank04052006@gmail.com>
2025-08-02 15:09:04 +0600

    рабочий аватар

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
